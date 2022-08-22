c7966dee3dd4e88b2d2c87d25ee585354b789c11
Rich <rrobertson@tentcraft.com>
2022-08-22 14:11:43 -0400

    lesson 9 - themes

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@
       >
         {{ link.label }}
       </v-btn>
-
+    <v-btn @click="toggleTheme" text rounded>Toggle Theme</v-btn>
     </v-app-bar>
     <v-content>
       <router-view></router-view>
@@ -61,6 +61,12 @@ export default {
         },
       ],
     };
+  },
+  methods: {
+    toggleTheme() {
+      this.$vuetify.theme.themes.dark.anchor = "#41B883";
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
   },
 };
 </script>
